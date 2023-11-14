f8b3eb629edbcdad91d76a252f6ccc9146b570cc
lciullo <lisa.ciullo42@gmail.com>
2023-11-14 14:20:38 +0100

    add exercice 01

--- ex01/sources/ScavTrap.cpp
+++ ex01/sources/ScavTrap.cpp
@@ -2,18 +2,18 @@
 
 ScavTrap::ScavTrap(void): ClapTrap()
 {
-	_HitPoints = 100;
-	_EnergyPoints = 50;
-	_AttackDamage = 20;
+	this->_HitPoints = 100;
+	this->_EnergyPoints = 50;
+	this->_AttackDamage = 20;
 	std::cout << "ScavTrap : default constructor called" << std::endl;
 	return ;
 }
 
 ScavTrap::ScavTrap(const std::string Name): ClapTrap(Name) 
 {
-	_HitPoints = 100;
-	_EnergyPoints = 50;
-	_AttackDamage = 20;
+	this->_HitPoints = 100;
+	this->_EnergyPoints = 50;
+	this->_AttackDamage = 20;
 	std::cout << "ScavTrap : string Name constructor called" << std::endl;
 	return ;
 }
@@ -44,3 +44,20 @@ ScavTrap &ScavTrap::operator=(const ScavTrap &other)
 	return (*this);
 }
 
+void	ScavTrap::guardGate(void)
+{
+	std::cout << "ScavTrap : " << this->_Name << " is enter in a guardGate " << std::endl;
+	return ;
+}
+
+void ScavTrap::attack(const std::string &target)
+{
+	if ((this->_HitPoints <= 0) || (this->_EnergyPoints <= 0))
+	{
+		std::cout << "ClapTrap " << this->_Name << " can't attack." << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << this->_Name << " attacks " << target << " causing " << this->_AttackDamage << " points of damage ! "<< std::endl;
+	this->_EnergyPoints--;
+	return ;
+}
