920b6ee36d9fb8a2fdc5c2a300ff2d0cffa27dc0
Lisa Ciullo <lciullo@z3r7p5.42lyon.fr>
2023-11-22 12:56:27 +0100

    add cpp04 ex02

--- ex02/includes/Animal.hpp
+++ ex02/includes/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: lciullo <lciullo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:45:26 by lciullo           #+#    #+#             */
-/*   Updated: 2023/11/22 10:29:12 by lciullo          ###   ########.fr       */
+/*   Updated: 2023/11/22 12:55:38 by lciullo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ class Animal
 				Animal(std::string Type);
 				Animal(const Animal &other);
 				Animal &operator=(const Animal &other);
-				virtual ~Animal(void) = 0;
+				virtual ~Animal(void);
 				std::string	getType(void) const;
 				void SetType(const std::string & );
-				virtual void makeSound(void) const;
+				virtual void makeSound(void) const = 0;
 	protected : 
 				std::string _Type;
 
