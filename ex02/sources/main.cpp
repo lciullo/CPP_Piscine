22772744b091fb7c526872b0e629496d1dc0296d
lciullo <lisa.ciullo42@gmail.com>
2023-12-20 20:24:51 +0100

    ex02 : add two vector container largest and smalest

--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lisa <lisa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 11:23:36 by lciullo           #+#    #+#             */
-/*   Updated: 2023/12/20 17:51:11 by lisa             ###   ########.fr       */
+/*   Updated: 2023/12/20 20:17:24 by lisa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,25 @@ std::vector<int> SortingVector(std::vector<int>	first)
 {
 	
 	std::vector<std::pair<int, int> > pairsVector = splitAndPair(first);
+	std::vector<int>smalest;
+	std::vector<int>largest;
 	for (std::size_t i = 0; i < pairsVector.size(); ++i) 
 	{
         std::cout << "Pair " << i + 1 << ": (" << pairsVector[i].first << ", " << pairsVector[i].second << ")" << std::endl;
+		int firstNb = pairsVector[i].first;
+		int secondNb = pairsVector[i].second;
+		if (pairsVector[i].first > pairsVector[i].second)
+		{
+			largest.push_back(firstNb);
+			smalest.push_back(secondNb);
+		}
+		else
+		{
+			largest.push_back(secondNb);
+			smalest.push_back(firstNb);
+		}	
     }
+	printContainer(largest);
+	printContainer(smalest);
 	return (first);
 }
